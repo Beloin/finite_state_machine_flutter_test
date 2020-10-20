0b72683fa5415ca7cea43eafebc53fb4c996e8c9
Beloin Sena <beloin.rodrigues@gmail.com>
2020-10-20 11:52:05 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# finite_state_machine_test
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+# finite_state_machine_flutter_test
+
+Links que ajudam:
+(Wikipedia Programação Orientada a Automatos)
+[https://pt.wikipedia.org/wiki/Programa%C3%A7%C3%A3o_orientada_a_aut%C3%B4matos]
+
+(Explicação de Máquina deestados em software)
+[https://medium.com/datadriveninvestor/state-machine-design-pattern-why-how-example-through-spring-state-machine-part-1-f13872d68c2d]
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+enum States { Fechada, Aberta, Abrindo, Fechando }
+
+class MyApp extends StatelessWidget {
+  StreamController<String> _controller = new StreamController();
+  States currentState = States.Fechada;
+
+  StreamController<States> _stateController = new StreamController();
+
+  @override
+  Widget build(BuildContext context) {
+    _stateController.add(currentState);
+
+    var stream = _stateController.stream.asBroadcastStream()
+      ..listen((event) {
+        currentState = event;
+      });
+
+    return MaterialApp(
+      title: 'State Machine Brute Test',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Porta State Machine'),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlatButton(
+                child: Text('Abrir'),
+                onPressed: () => _abrir(),
+              ),
+              FlatButton(
+                child: Text('Fechar'),
+                onPressed: () => _fechar(),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Porta'),
+                  StreamBuilder<States>(
+                    stream: stream,
+                    initialData: currentState,
+                    builder: (context, snapshot) {
+                      return Text(snapshot.data.toString());
+                    },
+                  ),
+                  SizedBox(height: 5),
+                  StreamBuilder<String>(
+                    stream: _controller.stream,
+                    initialData: 'Está fechada',
+                    builder: (context, snapshot) {
+                      return Text(snapshot.data);
+                    },
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _abrir() async {
+    switch (currentState) {
+      case States.Aberta:
+        _controller.add('Já está aberta');
+        break;
+      case States.Abrindo:
+        _controller.add('Já está abrindo');
+        break;
+      case States.Fechada:
+        _controller.add('Abrindo');
+        _stateController.add(States.Abrindo);
+        await Future.delayed(Duration(seconds: 5));
+        _stateController.add(States.Aberta);
+        _controller.add('Abriu');
+        break;
+      case States.Fechando:
+        _controller.add('Espere Terminar de Fechar');
+        break;
+      default:
+    }
+  }
+
+  _fechar() async {
+    switch (currentState) {
+      case States.Aberta:
+        _controller.add('Fechando');
+        _stateController.add(States.Fechando);
+        await Future.delayed(Duration(seconds: 5));
+        _stateController.add(States.Fechada);
+        _controller.add('Fechou');
+        break;
+      case States.Abrindo:
+        _controller.add('Espere Terminar de Abrir');
+        break;
+      case States.Fechada:
+        _controller.add('Já está fechada');
+        break;
+      case States.Fechando:
+        _controller.add('Já está fechando');
+        break;
+      default:
+    }
+  }
+}
